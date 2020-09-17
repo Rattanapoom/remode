efab3fad71097187648632e18c843617a14cfc8e
remode <614259058@webmail.npru.ac.th>
2020-09-17 11:39:23 +0700

    updata database

--- cafe.sql
+++ cafe.sql
@@ -29,11 +29,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `menu` (
-  `menu_id` char(5) COLLATE utf8_thai_520_w2 NOT NULL,
-  `menu_name` varchar(255) COLLATE utf8_thai_520_w2 NOT NULL,
+  `menu_id` char(5) NOT NULL,
+  `menu_name` varchar(255)  NOT NULL,
   `mcategory_id` int(2) NOT NULL,
-  `mshop_id` char(5) COLLATE utf8_thai_520_w2 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_thai_520_w2;
+  `mshop_id` char(5)  NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
 
 --
 -- Dumping data for table `menu`
@@ -45,7 +45,7 @@ INSERT INTO `menu` (`menu_id`, `menu_name`, `mcategory_id`, `mshop_id`) VALUES
 ('M1001', 'มอคค่า', 1, '1'),
 ('M2001', 'ลาเต้', 1, '2'),
 ('M3001', 'มอคค่า', 1, '3');
-
+('M4001', 'คาปูปีโป้', 1, '4')
 --
 -- Indexes for dumped tables
 --
